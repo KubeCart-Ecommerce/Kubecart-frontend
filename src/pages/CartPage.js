ce3e41b34c5cec6b17965b8ec9a1b0ced2c7fe02
Ahamed Aasik Afridi S <Ahamedaasikafridi.S@ust.com>
2026-04-26 21:00:50 +0530

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCart } from '../contexts/CartContext';
 import { orderApi } from '../services/api';
-// import { useAuth } from '../contexts/AuthContext';
 import toast from 'react-hot-toast';
 
 const CartPage = () => {
